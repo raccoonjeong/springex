62ad11b83fc13309bc3f7ada1ff2aba7685acfbb
raccoonjeong <rana3782@gmail.com>
2023-08-20 18:49:52 +0900

    Initial commit

---
+++ src/main/java/org/raccoon/springex/sample/SampleDAO.java
@@ -0,0 +1,4 @@
+package org.raccoon.springex.sample;
+
+public class SampleDAO {
+}
---
+++ src/main/java/org/raccoon/springex/sample/SampleService.java
@@ -0,0 +1,11 @@
+package org.raccoon.springex.sample;
+
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ToString
+public class SampleService {
+
+    @Autowired
+    private SampleDAO sampleDAO;
+}
